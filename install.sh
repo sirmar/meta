368c2738d98e822ff4af650fd65ce6663f90f5f9
Marcus Veibäck <sirmar@gmail.com>
2018-02-26 20:26:05 +0100

    Also install for linux

--- install.sh
+++ install.sh
@@ -1,16 +1,25 @@
 #!/bin/bash -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ARCH=`uname -s`
 
 docker build --quiet --tag meta:latest $DIR
 
-docker run --rm \
+if [ $ARCH == 'Darwin' ]; then
+   docker run --rm \
        -v "$DIR":/go/src/meta \
        -v "$DIR"/installed:/go/bin \
        -e GOOS=darwin \
        -e GOARCH=386 \
        meta:latest \
        go build -o /go/bin/meta
+else
+   docker run --rm \
+       -v "$DIR":/go/src/meta \
+       -v "$DIR"/installed:/go/bin \
+       meta:latest \
+       go build -o /go/bin/meta
+fi
 
 if [ ! -f /usr/local/bin/meta ]; then
     ln -sf ~/Code/meta/installed/meta /usr/local/bin/meta
